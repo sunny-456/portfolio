ad82eb3d5bb635380085a4b4f97d12c682f328ab
sunny-456 <skinder.hayat72@gmail.com>
2021-10-01 19:20:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function hiFun() {
+    console.log("ii");
+}
+function sendEmail() {
+    Email.send({
+        Host : "smtp.mailtrap.io",
+        Username : "<Mailtrap username>",
+        Password : "<Mailtrap password>",
+        To : 'recipient@example.com',
+        From : "sender@example.com",
+        Subject : "Test email",
+        Body : "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
+    }).then(
+      message => alert(message)
+    );
